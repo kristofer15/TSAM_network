b3a0a1aaafb54878007d38d447bf8669fb8a7e09
Hrafnkell NK <hrafnkellnk15@ru.is>
2018-10-21 18:58:37 +0000

    command line argument handling

--- server.cpp
+++ server.cpp
@@ -1,7 +1,19 @@
 #include "network_handler.h"
 
+void help(std::string file_name="server") {
+    std::cout << "Usage:" << std::endl
+              << file_name << " {TCP PORT} {UDP PORT}" << std::endl;
+}
+
+
 int main(int argc, char* argv[]) {
-    NetworkHandler handler;
+
+    if(argc < 3) {
+        help(argv[0]);
+        return 0;
+    }
+
+    NetworkHandler handler(atoi(argv[1]), atoi(argv[2]));
 
     handler.monitor_sockets();
 }
