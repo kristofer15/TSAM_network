d54361c5185d1a17d9dc7b697f57db0452c256d9
kristofer <kristofer@tern.is>
2018-10-23 11:41:01 +0000

    Fixing annoying typo

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 user=$(cat .skeluser)
 skel=$(cat .skeladdress)
 
-# Remove existing deployments an ignore errors
+# Remove existing deployments and ignore errors
 ssh -t $user@$skel rm -rf TSAM_botnet_G51 2> /dev/null
 
 # Copy current directory to server
