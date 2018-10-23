c5938822cbafa7f1829447a3b5451df69b4620bf
kristofer <kristofer@tern.is>
2018-10-23 11:40:44 +0000

    Deployment script improvments

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 user=$(cat .skeluser)
 skel=$(cat .skeladdress)
-ssh -t $user@$skel rm -rf TSAM_botnet
-scp -r $(pwd) $user@$skel:TSAM_botnet
+
+# Remove existing deployments an ignore errors
+ssh -t $user@$skel rm -rf TSAM_botnet_G51 2> /dev/null
+
+# Copy current directory to server
+scp -r $(pwd) $user@$skel:TSAM_botnet_G51
