725a0b262de48f1f0e00f5a75a9c600d1927159f
Hrafnkell NK <hrafnkellnk15@ru.is>
2018-10-29 21:26:25 +0000

    removed debug and send listroutes rsp to all clients

--- server.cpp
+++ server.cpp
@@ -10,19 +10,16 @@ void help(std::string file_name="server") {
 
 int main(int argc, char* argv[]) {
 
-    if(argc < 4) {
+    if(argc < 3) {
         help(argv[0]);
         return 0;
     }
 
-    NetworkHandler network(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+    NetworkHandler network(atoi(argv[1]), atoi(argv[2]));
     UserHandler users;
     AccessControl access;
 
-    // DEBUG
-    std::string id = "V_Group_51";
-    if(argc == 5) { id = argv[4]; }
 
-    MainController controller(network, users, access, id);
+    MainController controller(network, users, access);
     controller.run();
 }
